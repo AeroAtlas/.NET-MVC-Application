14822ee7d3d06f24bbed76a97d338c99916af3a3
Andrew Hardemon <aeroatlas@ymail.com>
2020-08-04 22:46:57 -0700

    added basic file structure for C#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _NET_MVC_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
